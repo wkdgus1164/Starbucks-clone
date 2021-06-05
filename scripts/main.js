f7197202dd3435ea5aceb5259d7ad1c5087558cf
Han Jang Hyun <wkdgus116490@gmail.com>
2021-06-05 19:23:23 +0900

    main banner gsap

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 const searchElement = document.querySelector('.search');
 const searchInputElement = searchElement.querySelector('input');
 const badgeElement = document.querySelector('header .badges');
+const fadeElements = document.querySelectorAll('.visual .fade-in');
 
 const handleWindowScroll = () => {
   console.log(window.scrollY);
@@ -35,3 +36,9 @@ window.addEventListener('scroll', _.throttle(handleWindowScroll, 300));
 searchElement.addEventListener('click', handleSearchElementClick);
 searchInputElement.addEventListener('focus', handleSearchInputElementFocus);
 searchInputElement.addEventListener('blur', handleSearchInputElementBlur);
+fadeElements.forEach((fadeElement, index) => {
+  gsap.to(fadeElement, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1,
+  });
+});
