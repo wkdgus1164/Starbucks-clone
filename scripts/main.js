c22076af14a14c7d535f3f61b5421984201ce73c
Han Jang Hyun <wkdgus116490@gmail.com>
2021-06-05 20:02:08 +0900

    swiperjs

--- scripts/main.js
+++ scripts/main.js
@@ -42,3 +42,9 @@ fadeElements.forEach((fadeElement, index) => {
     opacity: 1,
   });
 });
+
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true,
+});
